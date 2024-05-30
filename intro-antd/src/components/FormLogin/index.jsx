209a325183e83d14de49eb6ab06b4da0d6ff9ef6
Aura Renata <reny13ugalde@gmail.com>
2024-05-29 21:40:04 -0600

    feat(auth): Add axios for login and registration. Added axios to handle APU request for user login and registration

--- intro-antd/src/components/FormLogin/index.jsx
+++ intro-antd/src/components/FormLogin/index.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button, Card, message } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './FormLogin.css'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 const FormLogin = () => {
-    const onFinish = (values) => {
-        console.log('Success:', values);
+
+    const navigate = useNavigate();
+
+    //useState es menajeo de estado (error y carga)
+    const [loginError, setLoginError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+
+    const onFinish = async (values) => {
+        setLoading(true); //Establece el estado de carga a true al enviar el formulario
+        //console.log('Success:', values);
+        try {
+            const response = await axios.post('https://lizard-server.vercel.app/api/auth/signin', {
+                email: values.username, //Para este caso el email actua como el nombre de usuario
+                password: values.password
+            });
+            console.log('Inicio de sesión exitoso:', response.data);
+            localStorage.setItem('token', response.data.token) //Se guarda el token en almacenamiento
+            navigate('/'); //redirige al home
+        } catch (error) {
+            if (error.response) {
+                console.error('Error en el inicio de sesión:', error.response.data);
+            } else {
+                console.error('Error en el inicio de sesión:', error.message);
+            }
+            setLoginError(true);
+        } finally {
+            setLoading(false); //Establece el estado de carga a false despues de recibir la respuesta
+        }
     }
     const onFinishFailed = (errorInfo) => {
         console.log('Failed', errorInfo);
+        setLoginError(true);
     }
     return (
         <>
@@ -23,6 +53,7 @@ const FormLogin = () => {
                         remember: true,
                     }}
                     onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
                 >
 
                     <Form.Item
@@ -49,13 +80,14 @@ const FormLogin = () => {
                         <Input.Password prefix={<LockOutlined />} placeholder='Contraseña' />
                     </Form.Item>
                     <Form.Item>
-                        <Button type='primary' htmlType='submit' className='login-form-button'>
+
+                        {loginError && <p style={{ color: 'red' }}>Credenicales incorrecras. Intentalo de nuevo</p>}
+                        <Button type='primary' htmlType='submit' className='login-form-button' loading={loading}>
                             Iniciar Sesión
                         </Button>
                     </Form.Item>
                     ¿Aún no tienes cuenta? <a href='/register'>Registrate</a>
                 </Form>
-
             </Card>
         </>
     );
--- intro-antd/src/components/FormRegister/index.jsx
+++ intro-antd/src/components/FormRegister/index.jsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button, Card, message } from 'antd';
 import { UserOutlined, LockOutlined, MailFilled, MailOutlined } from '@ant-design/icons';
 import '../FormRegister'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 const FormRegister = () => {
-    const onFinish = (values) => {
-        console.log('Success:', values);
+
+    const navigate = useNavigate();
+
+    //Estado de error de registro
+    const [registroError, setRegisterError] = useState(false);
+    //Estado de carga 
+    const [loading, setLoading] = useState(false);
+
+    const onFinish = async (values) => {
+        setLoading(true);
+        //console.log('Success:', values);
+        try {
+            const response = await axios.post('https://lizard-server.vercel.app/api/auth/signup', {
+                username: values.username,
+                email: values.email,
+                password: values.password,
+                roles: ['moderador']
+            });
+            console.log('Registro exitoso:', response.data);
+            navigate('/login');
+        } catch (error) {
+            if (error.response) {
+                console.error('Error en el registro:', error.response.data);
+            } else {
+                console.error('Error en el registro:', error.message);
+            }
+            setRegisterError(true);
+        } finally {
+            setLoading(false); //Establece el estado de carga a false despues de recibir cualquier respuesta
+        }
     }
     const onFinishFailed = (errorInfo) => {
         console.log('Failed', errorInfo);
+        setRegisterError(true);
     }
+
+    //Recibe el argumento que se utiliza para obtener el valor de un campo específico del formulario
+    const validatePassword = ({ getFieldValue }) => ({
+        validator(_, value) {
+            if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+            }
+            return Promise.reject(new Error('Las contraseñas no coinciden'));
+        }
+    })
+
     return (
         <>
             <Card
@@ -18,11 +61,12 @@ const FormRegister = () => {
             >
                 <Form
                     name='normal_login'
-                    className='login-form'
+                    className='register-form'
                     initialValues={{
                         remember: true,
                     }}
                     onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
                 >
 
                     <Form.Item
@@ -39,19 +83,7 @@ const FormRegister = () => {
 
 
                     <Form.Item
-                        name='password'
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Por favor ingrese su contraseña'
-                            }
-                        ]}
-                    >
-                        <Input.Password prefix={<LockOutlined />} placeholder='Contraseña' />
-                    </Form.Item>
-
-                    <Form.Item
-                        name='mail'
+                        name='email'
                         rules={[
                             {
                                 required: true,
@@ -66,9 +98,36 @@ const FormRegister = () => {
                         <Input prefix={<MailOutlined />} placeholder='Correo electrónico' />
                     </Form.Item>
 
+                    <Form.Item
+                        name='password'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Por favor ingrese su contraseña'
+                            }
+                        ]}
+                    >
+                        <Input.Password prefix={<LockOutlined className='site-form-item-icon' />} type='password' placeholder='Contraseña' />
+                    </Form.Item>
+
+
+                    <Form.Item
+                        name='password-repeat'
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Confirme su contraseña'
+                            },
+                            ({ getFieldValue }) => validatePassword({ getFieldValue }),
+                        ]}
+                    >
+                        <Input.Password prefix={<LockOutlined className='site-form-item-icon' />} type='password' placeholder='Confirme contraseña' />
+                    </Form.Item>
+
                     <Form.Item>
-                        <Button type='primary' htmlType='submit' className='login-form-button'>
-                            Iniciar Sesión
+                        {registroError && <p style={{ color: 'red' }}>Fallo en el registro</p>}
+                        <Button type='primary' htmlType='submit' className='login-form-button' loading={loading}>
+                            Registrate
                         </Button>
                     </Form.Item>
                     ¿Ya tienes cuenta? <a href='/login'>Inicia sesión</a>
